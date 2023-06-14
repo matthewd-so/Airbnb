26346486e4c2787e35a6c42576b6e3ea0a5743d9
matthewd-so <61816265+matthewd-so@users.noreply.github.com>
2023-06-14 01:58:57 -0400

    Update sanity.cli.js

--- sanity.cli.js
+++ sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'q8ax68hi',
+    projectId: 'INPUT YOUR ID',
     dataset: 'production'
   }
 })
